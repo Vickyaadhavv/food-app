335858b2066520f9a166b06ae8e3587b11c69a24
Vickyaadhavv <102547223+Vickyaadhavv@users.noreply.github.com>
2022-07-07 19:55:11 +0530

    add

---
+++ src/categories.js
@@ -0,0 +1,109 @@
+const Categories =[
+    {
+        id:1,
+        title: "Dosa",
+        type:"Vegitarian" ,
+        price: 50,
+        image:"../image/name"
+        },
+        {
+         id:2,
+         title: "idly",
+         type:"Vegitarian" ,
+         price: 50,
+         image:"../image/name"
+            },
+            {
+           id:3,
+           title: "veg-biriyani",
+           type:"Vegitarian" ,
+            price: 100,
+           image:"../image/name"
+            },
+    {
+         id:4,
+         title: "chapathi",
+         type:"Vegitarian" ,
+         price: 50,
+         image:"../image/name"
+   },
+  {
+     id:5,
+     title: "parrotta",
+     type:"Vegitarian" ,
+     price: 40,
+     image:"../image/name"
+     },
+     {
+        id:6,
+        title: "chicken biriyani",
+        type:"non-Veg" ,
+        price: 150,
+        image:"../image/name"
+        },
+        {
+         id:7,
+         title: "mutton biriyani",
+         type:"non-Veg" ,
+         price: 150,
+         image:"../image/name"
+            },
+            {
+           id:8,
+           title: "Mutton sukka",
+           type:"non-Veg" ,
+            price: 350,
+           image:"../image/name"
+            },
+    {
+         id:9,
+         title: "chicken manjurian",
+         type:"non-Veg" ,
+         price: 289,
+         image:"../image/name"
+   },
+  {
+     id:10,
+     title: "Dragon chicken",
+     type:"non-Veg" ,
+     price: 280,
+     image:"../image/name"
+     },
+     {
+        id:11,
+        title: "Classic Brownie",
+        type:"desserts" ,
+        price: 149,
+        image:"../image/name"
+        },
+        {
+         id:12,
+         title: "Red Velvet Brownie",
+         type:"desserts" ,
+         price: 169,
+         image:"../image/name"
+            },
+            {
+           id:13,
+           title: "Dark Chocolate Brownie",
+           type:"desserts" ,
+            price: 159,
+           image:"../image/name"
+            },
+    {
+         id:14,
+         title: "Milk Chocolate Brownie",
+         type:"desserts" ,
+         price: 159,
+         image:"../image/name"
+   },
+  {
+     id:15,
+     title: "White Choclate Brownie",
+     type:"desserts" ,
+     price: 159,
+     image:"../image/name"
+     },
+]
+
+export default Categories;
---
+++ src/category.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./App.css";
+import Categories from "./categories";
+
+const Category = () => {
+    const [data ,setData]= useState(Categories);
+    const filterResult =(curItem)=>{
+        const result =Categories.filter((curData)=>{
+            return curData.type===curItem;
+        })
+        setData(result)
+    }
+    return (
+        <>
+        <h1 className="header"> Let's order Food</h1>
+        <div className="container">
+              <div className="buttons">
+                <button className="btn" onClick={()=>setData(Categories)}>Recommended</button>
+                <button className="btn" onClick={()=>filterResult("Vegitarian")}>Veg</button>
+                <button className="btn" onClick={()=>filterResult("non-Veg")}>Non-veg</button>
+                <button className="btn"  onClick={()=>filterResult("desserts")}>Desserts</button>
+              </div>
+              <div className="right">
+                <div className="cards-content">
+                {data.map((values)=>{
+                    const {id,title,price,type,image}= values
+                    return(
+                        <>
+                    <div className="cards" key={id}>
+                       <h2 className="title">{title}</h2>
+                        <p>{type}</p>
+                        <p className="amt">{price}</p>
+                        <img src="{image}" alt="" />
+                        <button className="btn-cart-1">Remove Cart</button>
+                        <button className="btn-cart-2">Add Cart</button>
+                    </div>
+                    
+                        </>
+                    )
+                })}
+                </div>
+              </div>
+              </div>
+              
+            </>
+    )
+}
+
+export default Category;
